9b498acff48ff4a02de210d8b247e9b54a41cf7e
seankim <seandoesdev@gmail.com>
2024-01-15 19:56:52 +0900

    initial commit

---
+++ src/main/java/com/shop/puppyPet/PuppyPetApplication.java
@@ -0,0 +1,13 @@
+package com.shop.puppyPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/puppyPet/PuppyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.puppyPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
